5a4a9d15110a72a4d22efb8d52d529764e65e0ee
Fred K. Schott <fkschott@gmail.com>
2019-01-21 17:22:52 -0800

    add types pkg

---
+++ packages/types/pkg/dist-node/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+class MessageError extends Error {
+  constructor(msg) {
+    super(msg);
+  }
+
+}
+
+exports.MessageError = MessageError;
---
+++ packages/types/pkg/dist-types/index.d.ts
@@ -0,0 +1,24 @@
+export declare type BuilderOptions = {
+    cwd: string;
+    out: string;
+    options: any;
+    src: {
+        loc: string;
+        entrypoint: string;
+        options: any;
+        files: Array<string>;
+    };
+    watch: string | undefined;
+    reporter: {
+        info: (msg: string) => void;
+        warning: (msg: string) => void;
+        success: (msg: string) => void;
+        created: (file: string, entrypoint?: string) => void;
+    };
+    isFull: boolean;
+    manifest: any;
+    rollup: any;
+};
+export declare class MessageError extends Error {
+    constructor(msg: string);
+}
